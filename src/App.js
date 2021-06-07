460e4a12b93dcf6d80abaaf7f2f85dd3a89dddd1
mohamedMok <mohamed.mokhtari.pro@gmail.com>
2021-06-07 11:26:06 +0200

    feat(pattern): test mozaic button

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import '@mozaic-ds/mozaic-web-components/public/bundle.js';
+import '@mozaic-ds/mozaic-web-components/public/icons.js';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <mc-button label="Mozaic Button" iconleft="true"> 
+        <user-account-customer--collaborator-24px slot="iconLeft" />
+      </mc-button>
+      <user-account-customer--collaborator-64px />
     </div>
   );
 }
